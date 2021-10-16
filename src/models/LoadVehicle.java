9ea21014b8fbf5a3c783b9a482d31c02cb7c2749
Steili Roland <steiliroland2001@gmail.com>
2021-10-16 17:16:56 +0200

    dolgozat

--- src/models/LoadVehicle.java
+++ src/models/LoadVehicle.java
@@ -1,13 +1,14 @@
 /*
  * File: LoadVehicle.java
  * Created Date: 2021-09-25 00:46:41
- * Author: Sallai Andras
+ * Author: Steili Roland
+ * Original: Sallai Andras
  * Github: https://github.com/andteki
  * -----
- * Last Modified: 2021-09-25
- * Modified By: Sallai Andras
+ * Last Modified: 2021-10-16
+ * Modified By: Steili Roland
  * -----
- * Copyright (c) 2021 Sallai Andras
+ * Copyright (c) 2021 Steili Roland
  * 
  * GNU GPL v2
  */
@@ -23,15 +24,13 @@ public class LoadVehicle {
     ArrayList<String> vehicles;
     public LoadVehicle() {
         vehicles = new ArrayList<>();
-    }//A LoadVehicle metódus vége
 
     public ArrayList<Vehicle> load(){
-    //A járművek listáját tárlojuk benne:
     ArrayList<Vehicle> vehicleList = new ArrayList<>();
     try {
     FileReader fileReader = new FileReader("data.txt");
     Scanner scanner = new Scanner(fileReader);
-    //Ciklus amivel bejárjuk a fájlt:
+    
     while(scanner.hasNext()) {
     String line = scanner.nextLine();
     String[] lineArray = line.split(":");
@@ -40,12 +39,12 @@ public class LoadVehicle {
     vehicle.brand = lineArray[1];
     vehicle.year = lineArray[2];
     vehicleList.add(vehicle);
-    }//while vége
+    }
     scanner.close();
 
     } catch (FileNotFoundException e) {
     System.err.println("Hiba! A fájl nem található");
-    }//A try vége
+    }
     return vehicleList;
     }
 }
